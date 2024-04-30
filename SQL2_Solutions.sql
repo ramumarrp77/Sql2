51a5a7cc02404d71204f7f0527ec245ed05043a7
ramumarrp77 <58823379+ramumarrp77@users.noreply.github.com>
2024-04-29 22:54:15 -0400

    Adding comments

--- SQL2_Solutions.sql
+++ SQL2_Solutions.sql
@@ -1,11 +1,13 @@
-1)
+--SQL 2 SOlutions
+
+--1)Rank scores - Problem No :- 4
 select 
 score, 
 dense_rank() over (order by score desc) as 'rank'
 from scores;
 
 
-2) 
+--2) Exchange seats - Problem No :- 5
 select  id,
 case when id%2 =1 then ifnull(lead(student) over (),student)
 when id%2 = 0 then lag(student) over()
@@ -13,7 +15,7 @@ else student end as student
  from seat;
 
 
-3) 
+--3) Tree Node  - Problem No :- 6
 select 
 t1.id,case when t1.p_id is null then 'Root'
 when t1.p_id is not null and t2.p_id is not null then 'Inner'
@@ -24,8 +26,7 @@ group by 1,2;
 
 
 
-4) 
-
+-- 4) Department Top 3 Salaries - Problem No :- 7
 select department, employee, Salary from
 (select d.name as department, e.name as employee, Salary,
 dense_rank() over (partition by d.name order by salary desc)  as rnk from Employee e
